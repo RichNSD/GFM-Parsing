f947c843ec09bd53a953f69e7f3364605e691503
Richard Joubert <rjoubert@nsd.team>
2022-12-17 17:13:13 -0500

    update

--- lib/gfm-parser.rb
+++ lib/gfm-parser.rb
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
 require 'kramdown/options'
+require 'kramdown/parser/kramdown'
+
+require_relative 'gfm/options'
+require_relative 'gfm_version'
+
+module Kramdown
+	module Parser
+		
+		
+
+

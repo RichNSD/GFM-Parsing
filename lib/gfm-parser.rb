81539c4b241f560c5bfbe2180141dd422a6d3335
Richard Joubert <rjoubert@nsd.team>
2022-12-25 12:18:00 -0500

    established dependencies
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

--- lib/gfm-parser.rb
+++ lib/gfm-parser.rb
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
-require 'kramdown/options'
-require 'kramdown/parser/kramdown'
-
-require_relative 'gfm/options'
-require_relative 'gfm_version'
-
-module Kramdown
-	module Parser
-		
-		
+require 'rubygems'
+require 'github-markdown-server'	'~> 1.0', '>= 1.0.1'
+require 'github-markdown-preview',	'~> 4.0'
+require 'github-markdown',			'= 0.6.9'
+require 'github-linguist',			'= 5.0.4'
+require 'commonmarker',				'~> 0.23.4'
+require 'gemoji',					'= 3.0.0'
+require 'html-pipeline',			'= 2.7.1'
+require 'listen',					'= 3.1.5'
+require 'sanitize',					'~> 5.2.1'
+require 'cgi',						'>= 0'
+require 'charlock_holmes',			'~> 0.7.7'
+require 'mini_mime',				'~> 1.0'
+require 'rugged',					'~> 1.0'
 
 
